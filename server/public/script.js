e9b77a6b20718b64973371320b88c96290a6e490
mgm <tommin231@gmail.com>
2020-08-17 00:05:41 +0900

    added comments

--- server/public/script.js
+++ server/public/script.js
@@ -1,34 +1,57 @@
 const socket = io('/');
 const videoGrid = document.getElementById('video-grid');
+
+// initialize Peer object with no arguments to user peerJS's server
 const myPeer = new Peer();
+// video element to capture video from webcam
 const myVideo = document.createElement('video');
 myVideo.muted = true;
+
+/** object to store connected users
+ * @example
+ * {userId : call object,}
+ * */
+
 const peers = {};
+
+// access video from webcam
 navigator.mediaDevices
   .getUserMedia({
     video: true,
     audio: true,
   })
   .then((stream) => {
+    // add video stream from webcam to video element
     addVideoStream(myVideo, stream);
 
+    // handle connection between user via Peer object
     myPeer.on('call', (call) => {
       call.answer(stream);
+
+      // create new video element to append to video grid
       const video = document.createElement('video');
       call.on('stream', (userVideoStream) => {
         addVideoStream(video, userVideoStream);
       });
     });
 
+    // when server emit user-connected
     socket.on('user-connected', (userId) => {
       connectToNewUser(userId, stream);
     });
   });
 
+/** when socket receive 'user-disconnected'
+ * 1. get call object from peers
+ * 2. close connection via call.close()
+ * */
 socket.on('user-disconnected', (userId) => {
   if (peers[userId]) peers[userId].close();
 });
 
+/** on Peer open send emit 'join-room via socket
+ * userId is created by Peer
+ * */
 myPeer.on('open', (id) => {
   socket.emit('join-room', ROOM_ID, id);
 });
@@ -43,9 +66,14 @@ function connectToNewUser(userId, stream) {
     video.remove();
   });
 
+  // add call object to peers
   peers[userId] = call;
 }
 
+/**this function
+ * 1. add video stream to video element
+ * 2. append video element to video grid
+ */
 function addVideoStream(video, stream) {
   video.srcObject = stream;
   video.addEventListener('loadedmetadata', () => {
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+// import express, ejs, uuid
 const express = require('express');
 const app = express();
 const { v4: uuidV4 } = require('uuid');
 
+// set up https, via letsencrypt
 const fs = require('fs');
 const https = require('https');
 const server = https.createServer(
@@ -15,28 +17,37 @@ const server = https.createServer(
   app
 );
 
+// import socket io
 const io = require('socket.io')(server);
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+// redirect to random room url
 app.get('/', (req, res) => {
   res.redirect(`/${uuidV4()}`);
 });
 
+// roomId from url
 app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room });
 });
 
 io.on('connection', (socket) => {
+  // when user join-room
   socket.on('join-room', (roomId, userId) => {
+    // add user to room
     socket.join(roomId);
+    // broadcast to other users in same room : user-connected
     socket.to(roomId).broadcast.emit('user-connected', userId);
 
+    // on disconnect
     socket.on('disconnect', () => {
+      // broadcast to other user ins same room : user-disconnected
       socket.to(roomId).broadcast.emit('user-disconnected', userId);
     });
   });
 });
 
+// set port number
 server.listen(portNumber);
